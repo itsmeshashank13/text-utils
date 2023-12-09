f4ee0c9615c92cf056ffd7c1cb89d33382394901
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-12-09 16:05:11 +0530

    created 1 feature of making text uppercase

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Textform(props) {
+
+  const handleUppercaseClick = ()=> {
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleOnChange = (event)=> {
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState("");
+
   return (
     <div>
       <div className="mb-3">
-        <label htmlFor="myBox" class="form-label"><h4>{props.heading}</h4></label>
-        <textarea className="form-control" id="myBox" rows="8" />
+        <label htmlFor="myBox" className="form-label">
+          <h3>{props.heading}</h3>
+        </label>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" placeholder="Your text goes here" rows="8" />
       </div>
-      <button className="btn btn-primary">Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleUppercaseClick}>Convert to Uppercase</button>
     </div>
-  );
-}
+  )
+};
